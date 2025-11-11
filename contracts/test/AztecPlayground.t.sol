ca5a9969aeb6735613f06fb302e106afff76c8c9
filip <filip.harald@gmail.com>
2025-11-11 10:09:24 +0100

    updated aztec contracts

--- contracts/test/AztecPlayground.t.sol
+++ contracts/test/AztecPlayground.t.sol
@@ -1,10 +1,22 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2025 Aztlan Labs
 
-import {IRollup} from "@az/core/interfaces/IRollup.sol";
+// import {IRollup} from "@az/core/interfaces/IRollup.sol";
 import {IRegistry} from "@az/governance/interfaces/IRegistry.sol";
-import {IInbox} from "@az/core/interfaces/messagebridge/IInbox.sol";
-import {IOutbox} from "@az/core/interfaces/messagebridge/IOutbox.sol";
-import {DataStructures} from "@az/core/libraries/DataStructures.sol";
 
-// TODO
+// import {IInbox} from "@az/core/interfaces/messagebridge/IInbox.sol";
+// import {IOutbox} from "@az/core/interfaces/messagebridge/IOutbox.sol";
+// import {DataStructures} from "@az/core/libraries/DataStructures.sol";
+
+contract AztecPlayground {
+    IRegistry public registry;
+
+    constructor(address _registry) {
+        registry = IRegistry(_registry);
+        registry.addRollup(address(this));
+    }
+
+    function getRollupAddress() external view returns (address) {
+        return registry.getRollupAddress();
+    }
+}
