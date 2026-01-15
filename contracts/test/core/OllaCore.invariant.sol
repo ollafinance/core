1e73b285b461281e8213fae147ffcf0b682ee24e
WietzeS <wietze@kryha.io>
2026-01-15 12:03:35 +0100

    add invariant tests

--- contracts/test/core/OllaCore.invariant.sol
+++ contracts/test/core/OllaCore.invariant.sol
@@ -81,6 +81,55 @@ contract OllaCoreHandler is Test {
     }
 }
 
+contract OllaCoreDepositHandler is Test {
+    using Math for uint256;
+
+    MockAztec public asset;
+    OllaCore public vault;
+    StAztec public stAztec;
+
+    address[] public actors;
+
+    uint256 public previousExchangeRate;
+    uint256 public lastExchangeRate;
+
+    constructor(MockAztec _asset, OllaCore _vault, StAztec _stAztec) {
+        asset = _asset;
+        vault = _vault;
+        stAztec = _stAztec;
+
+        for (uint256 i = 0; i < 5; i++) {
+            actors.push(makeAddr(string(abi.encode("actor", i))));
+        }
+
+        lastExchangeRate = vault.exchangeRate();
+        previousExchangeRate = lastExchangeRate;
+    }
+
+    function actorsLength() external view returns (uint256) {
+        return actors.length;
+    }
+
+    function actorAt(uint256 index) external view returns (address) {
+        return actors[index];
+    }
+
+    function deposit(uint96 amount, uint256 actorSeed) external {
+        previousExchangeRate = lastExchangeRate;
+
+        uint256 assets = uint256(bound(amount, 1, type(uint96).max));
+        address actor = actors[bound(actorSeed, 0, actors.length - 1)];
+
+        asset.mint(actor, assets);
+        vm.startPrank(actor);
+        asset.approve(address(vault), assets);
+        vault.deposit(assets, actor);
+        vm.stopPrank();
+
+        lastExchangeRate = vault.exchangeRate();
+    }
+}
+
 contract OllaCoreInvariantTest is Test {
     using Math for uint256;
 
@@ -117,5 +166,67 @@ contract OllaCoreInvariantTest is Test {
         uint256 expectedRate = supply == 0 ? 1e18 : vault.totalAssets().mulDiv(1e18, supply, Math.Rounding.Floor);
 
         assertEq(vault.exchangeRate(), expectedRate, "exchange rate matches totals");
+    }
+
+    function invariant_DepositFormulaMatchesSpec() external view {
+        uint256 assets = 1e18;
+        uint256 supply = stAztec.totalSupply();
+        uint256 expectedShares = supply == 0 ? assets : assets.mulDiv(supply, vault.totalAssets(), Math.Rounding.Floor);
+
+        assertEq(vault.depositFormula(assets), expectedShares, "deposit formula matches spec");
+    }
+
+    function invariant_ZeroSupplyBehavior() external view {
+        uint256 supply = stAztec.totalSupply();
+        if (supply != 0) {
+            return;
+        }
+
+        uint256 assets = 1e18;
+        assertEq(vault.exchangeRate(), 1e18, "zero supply exchange rate");
+        assertEq(vault.depositFormula(assets), assets, "zero supply deposit formula");
+        assertEq(vault.userValue(address(0xBEEF)), 0, "zero supply user value");
+    }
+}
+
+contract OllaCoreDepositInvariantTest is Test {
+    using Math for uint256;
+
+    OllaCore internal vault;
+    StAztec internal stAztec;
+    MockAztec internal asset;
+    MockStakingManager internal stakingManager;
+    OllaCoreDepositHandler internal handler;
+
+    function setUp() external {
+        asset = new MockAztec(address(this));
+
+        OllaCore implementation = new OllaCore();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), "");
+        vault = OllaCore(address(proxy));
+
+        stAztec = new StAztec(address(vault));
+        stakingManager = new MockStakingManager();
+        vault.initialize(asset, stAztec, stakingManager);
+
+        handler = new OllaCoreDepositHandler(asset, vault, stAztec);
+        targetContract(address(handler));
+    }
+
+    function invariant_ExchangeRateMonotonicWithDeposits() external view {
+        assertGe(handler.lastExchangeRate(), handler.previousExchangeRate(), "exchange rate monotonic");
+    }
+
+    function invariant_UserValueMatchesFormula() external view {
+        uint256 rate = vault.exchangeRate();
+        uint256 actorCount = handler.actorsLength();
+
+        for (uint256 i = 0; i < actorCount; i++) {
+            address actor = handler.actorAt(i);
+            uint256 shares = stAztec.balanceOf(actor);
+            uint256 expectedValue = shares == 0 ? 0 : shares.mulDiv(rate, 1e18, Math.Rounding.Floor);
+
+            assertEq(vault.userValue(actor), expectedValue, "user value matches formula");
+        }
     }
 }
