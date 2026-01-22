f3c73a300d44fc8817c8219eec1a9d4d9da6daf7
filip <filip.harald@gmail.com>
2026-01-22 11:46:00 +0100

    pure

--- contracts/src/mocks/MockStakingManager.sol
+++ contracts/src/mocks/MockStakingManager.sol
@@ -43,30 +43,6 @@ contract MockStakingManager is IStakingManager {
         ++unstakeCalls;
     }
 
-    /// @inheritdoc IStakingManager
-    function addKeysToProvider(KeyStore[] calldata) external override {
-        // No-op for mock
-        return;
-    }
-
-    /// @inheritdoc IStakingManager
-    function dripQueue(uint256) external override {
-        // No-op for mock
-        return;
-    }
-
-    /// @inheritdoc IStakingManager
-    function setProviderRewardsRecipient(address) external override {
-        // No-op for mock
-        return;
-    }
-
-    /// @inheritdoc IStakingManager
-    function cleanActivatedAttesters() external override {
-        // No-op for mock
-        return;
-    }
-
     /*//////////////////////////////////////////////////////////////
                         EXTERNAL VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
@@ -113,5 +89,33 @@ contract MockStakingManager is IStakingManager {
     /// @inheritdoc IStakingManager
     function isUnstakePending(address) external pure override returns (bool) {
         return false;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        EXTERNAL NON-VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc IStakingManager
+    function addKeysToProvider(KeyStore[] calldata) external pure override {
+        // No-op for mock
+        return;
+    }
+
+    /// @inheritdoc IStakingManager
+    function dripQueue(uint256) external pure override {
+        // No-op for mock
+        return;
+    }
+
+    /// @inheritdoc IStakingManager
+    function setProviderRewardsRecipient(address) external pure override {
+        // No-op for mock
+        return;
+    }
+
+    /// @inheritdoc IStakingManager
+    function cleanActivatedAttesters() external pure override {
+        // No-op for mock
+        return;
     }
 }
