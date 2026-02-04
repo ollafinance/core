be5a2098a9b48a2997e9f5f5a31fb748b93d0775
WietzeS <wietze@kryha.io>
2026-02-04 15:30:56 +0100

    harvestedRewards > rewardsDelta

--- contracts/src/core/interfaces/IOllaCore.sol
+++ contracts/src/core/interfaces/IOllaCore.sol
@@ -155,11 +155,11 @@ interface IOllaCore {
     event AttestersStateRead(uint256 rewardsDelta, uint256 slashingDelta, uint256 timestamp);
 
     /// @notice Emitted when a rebalance operation completes.
-    /// @param harvestedAmount Amount of rewards harvested.
+    /// @param rewardsDelta Amount of rewards harvested.
     /// @param finalizedAmount Amount of assets used for withdrawal finalization.
     /// @param stakedAmount Amount of assets staked.
     /// @param resultingBuffer Final buffered assets after rebalance.
-    event Rebalanced(uint256 harvestedAmount, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
+    event Rebalanced(uint256 rewardsDelta, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
 
     /// @notice Emitted when unstaked funds are claimed during rebalance.
     /// @param amount The amount of unstaked funds received.
@@ -332,13 +332,13 @@ interface IOllaCore {
     function unpause() external;
 
     /// @notice Operator-triggered rebalance hook.
-    /// @return harvestedAmount The amount of rewards harvested.
+    /// @return rewardsDelta The amount of rewards harvested.
     /// @return finalizedAmount The amount of assets used for withdrawal finalization.
     /// @return stakedAmount The amount of assets staked.
     /// @return resultingBuffer The final buffered assets after rebalance.
     function rebalance()
         external
-        returns (uint256 harvestedAmount, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
+        returns (uint256 rewardsDelta, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
 
     /// @notice Operator-triggered accounting update hook.
     function updateAccounting() external;
