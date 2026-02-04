addf4245ebd8f4587deb3d1af046fd151f3cc3a4
filip <filip.harald@gmail.com>
2026-02-04 16:19:34 +0100

    fixed-some-stuff

--- contracts/src/core/interfaces/IOllaCore.sol
+++ contracts/src/core/interfaces/IOllaCore.sol
@@ -155,11 +155,11 @@ interface IOllaCore {
     event AttestersStateRead(uint256 rewardsDelta, uint256 slashingDelta, uint256 timestamp);
 
     /// @notice Emitted when a rebalance operation completes.
-    /// @param harvestedAmount Amount of rewards harvested.
+    /// @param rewardsDelta Amount of rewards harvested.
     /// @param finalizedAmount Amount of assets used for withdrawal finalization.
     /// @param stakedAmount Amount of assets staked.
     /// @param resultingBuffer Final buffered assets after rebalance.
-    event Rebalanced(uint256 harvestedAmount, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
+    event Rebalanced(uint256 rewardsDelta, uint256 finalizedAmount, uint256 stakedAmount, uint256 resultingBuffer);
 
     /// @notice Emitted when unstaked funds are claimed during rebalance.
     /// @param amount The amount of unstaked funds received.
